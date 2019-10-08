ad556f377d117e14bcf80210be1701b0fa1524c4
Ryan Conklin <ryan_carolina88@yahoo.com>
2019-10-08 15:37:49 -0400

    added a test class for SpaceCamp

---
+++ src/test/java/kaijuTests/SpaceCampTestClass.java
@@ -0,0 +1,45 @@
+package kaijuTests;
+
+import kaijuUtil.Kaiju;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SpaceCampTestClass {
+    public static Kaiju kaiju;
+
+    @Before
+    public void setUp(){
+        kaiju = new Kaiju("CHROME");
+    }
+
+    @Test
+    public void assertGoPageTitle(){
+        kaiju.setDimensionBrowserWindow(1440, 900);
+        kaiju.getUrl("http://localhost/?name=Felipe%20Ortiz");
+        kaiju.checkTitle("EXPANSIA - Deploy Technology Faster");
+    }
+
+    @Test
+    public void assertUrlNameParameter(){
+        kaiju.setDimensionBrowserWindow(1440, 900);
+        kaiju.getUrl("http://localhost/?name=Felipe%20Ortiz");
+        kaiju.checkForTextBySelector("body > div.container > p", "Felipe Ortiz");
+    }
+
+    @Test
+    public void assertAboutLinkNavigation(){
+        kaiju.setDimensionBrowserWindow(1440, 900);
+        kaiju.getUrl("http://localhost/?name=Felipe%20Ortiz");
+        kaiju.waitForElementVisibleByPartialLinkText("About", 20);
+        kaiju.clickPartialLink("About");
+
+        kaiju.waitForElementVisibleById("thb-image-5d9b9a6122904", 20);
+        kaiju.takeScreenShot();
+    }
+
+    @After
+    public void killKaiju(){
+        //kaiju.killKaijuDriver();
+    }
+}
